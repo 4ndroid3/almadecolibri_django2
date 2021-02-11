7ca7a349f6284855b6a6b14623a749ed7b407231
Jaimovich Andrés <35976464+4ndroid3@users.noreply.github.com>
2021-02-11 11:49:15 -0300

    Update models.py

--- tienda/models.py
+++ tienda/models.py
@@ -7,6 +7,31 @@ from productos.models import Producto
 
 
 class Venta(models.Model):
+    """ Clase que maneja las ventas realizadas por los usuarios.
+    
+    
+    
+    Contiene:
+    - ID
+    - ID_Usuario (trae el nombre del usuario que realizo la compra)
+    - Nombre Invitado (en caso de que se realice una venta como usuario 
+        no registrado este valor almacenará un nombre provisorio puesto
+        por el cliente)
+    - Apellido Invitado (en caso de que se realice una venta como usuario 
+        no registrado este valor almacenará un apellido provisorio puesto
+        por el cliente)
+    - Teléfono Invitado (en caso de que se realice una venta como usuario 
+        no registrado este valor almacenará un teléfono provisorio puesto
+        por el cliente)
+    - Precio total de la venta ( suma el valor de todos los productos comprados
+        en una venta)
+    - Venta Procesada (si el usuario finalizo la venta, el administrador ya puede
+        procesar el pedido y prepararlo, una vez preparado, se cambia el estado a
+        procesado y se le puede avisar al usuario que su pedido está listo)
+    - Venta finalizada (Cuando el usuario finaliza el pedido, este parametro
+        pasa a True, dandole la indicacion al administrador de que debe preparar
+        el pedido)
+    """
     id = models.AutoField(primary_key=True)
     id_usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     nombre_inv = models.CharField(max_length=150, blank=True)
@@ -25,6 +50,16 @@ class Venta(models.Model):
         return str(self.id)
         
 class Detalle_Venta(models.Model):
+    """ Clase que maneja los productos de cada pedido.
+    
+    Una vez que el usuario agrega un pedido al carrito de compra se relaciona 
+    cada Detalle de venta con un id_venta.
+    
+    - ID Venta (relaciona la venta individual con una ID de venta del usuario)
+    - ID Producto ( relaciona el producto con la venta individual)
+    - Cantidad Vendida (cantidad en gramos de el producto seleccionado)
+    - Precio Unitario (precio del producto cada 100gramos comprados)
+    """
     id_venta = models.ForeignKey(Venta, on_delete=models.CASCADE)
     id_producto = models.ForeignKey(Producto, on_delete=models.CASCADE, verbose_name='Producto')
     cant_vendida = models.PositiveIntegerField(verbose_name='Cantidad Vendida')
